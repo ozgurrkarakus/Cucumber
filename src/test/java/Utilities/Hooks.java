99c83302f40ff50ee7dffc4ee7d8d66d683702a2
ozgurrkarakus <ozgurkarakus.fb@gmail.com>
2024-03-19 21:19:13 +0300

    Hooks 2

--- src/test/java/Utilities/Hooks.java
+++ src/test/java/Utilities/Hooks.java
@@ -1,11 +1,22 @@
 package Utilities;
 
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
 
 public class Hooks {
+
+
+    @Before// Cucumber ın Annotation
+    public void before(){
+       // System.out.println("Sennaryo başladı");
+    }
+
+
+
     @After // Cucumber ın Annotation ı
     // her senaryodan sonra çalışır
     public void after(){
         GWD.quitDriver();
     }
+
 }
