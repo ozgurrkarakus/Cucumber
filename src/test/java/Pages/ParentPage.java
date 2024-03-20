deea71fbe2a1af18712430af8cb43b793784d2b8
ozgurrkarakus <ozgurkarakus.fb@gmail.com>
2024-03-20 19:49:10 +0300

    ParentPage 2

--- src/test/java/Pages/ParentPage.java
+++ src/test/java/Pages/ParentPage.java
@@ -2,7 +2,9 @@ package Pages;
 
 import Utilities.GWD;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -34,6 +36,8 @@ public class ParentPage {
     public void verifyContainsText(WebElement element, String value){
         wait.until(ExpectedConditions.textToBePresentInElement(element,value));
         Assert.assertTrue(element.getText().toLowerCase().equals(value.toLowerCase()));
+    new Actions(GWD.getDriver()).sendKeys(Keys.ESCAPE).build().perform();
+    //Sayfaya ESC Tuşu Gönderildi
     }
 
 }
