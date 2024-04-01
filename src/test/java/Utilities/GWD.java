d660956d48cd69b63341cbb5788d456845f980fa
ozgurrkarakus <ozgurkarakus.fb@gmail.com>
2024-04-01 20:14:24 +0300

    GWD new

--- src/test/java/Utilities/GWD.java
+++ src/test/java/Utilities/GWD.java
@@ -2,6 +2,8 @@ package Utilities;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 import java.util.Locale;
@@ -9,21 +11,33 @@ import java.util.Locale;
 
 public class GWD {
 
-    private static WebDriver driver;
+    private static ThreadLocal<WebDriver> threadDriver=new ThreadLocal<>();  // thread e özel driver
+    private static ThreadLocal<String> threadBrowserName=new ThreadLocal<>(); // o thread e özel browser name
 
-
+    //threadDriver.get() -> bulunduğum thread deki driver ı al
+    //threadDriver.set(driver) -> bulunduğum threade driver set et
     public static WebDriver getDriver(){
 
         Locale.setDefault(new Locale("EN"));
         System.setProperty("user.language", "EN");
 
-        if (driver == null) { //1 kez oluştur
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        if (threadBrowserName.get()==null) // XML den çalışmayan durumlar için
+            threadBrowserName.set("chrome");  // default chrome
+
+        if (threadDriver.get() == null) { //1 kez oluştur
+
+            switch (threadBrowserName.get())
+            {
+                case "firefox" : threadDriver.set(new FirefoxDriver()); break;
+                case "edge" : threadDriver.set(new EdgeDriver()); break;
+                default: threadDriver.set(new ChromeDriver());  //bulunduğum hatta driver yok idi, ben bir tane set ettim
+            }
+
+            threadDriver.get().manage().window().maximize();
+            threadDriver.get().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
         }
 
-        return driver;
+        return threadDriver.get();
     }
 
 
@@ -37,9 +51,13 @@ public class GWD {
         }
 
         //driver kapat
-        if (driver != null){ //driver var ise
-            driver.quit();
-            driver=null;
+        if (threadDriver.get() != null){ //bu hatta driver var ise
+
+            threadDriver.get().quit();
+
+            WebDriver driver= threadDriver.get(); //hattaki driverı aldım
+            driver=null;  // null haline getirdim
+            threadDriver.set(driver);//tekrar hatta atadım.
         }
 
     }
