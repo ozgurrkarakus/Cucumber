79d082fb6473ad5cb40a108f59178261aa65773a
ozgurrkarakus <ozgurkarakus.fb@gmail.com>
2024-04-02 19:32:33 +0300

    apache2

--- src/test/java/ApachePOI/_04_ApachePOIGetAllData.java
+++ src/test/java/ApachePOI/_04_ApachePOIGetAllData.java
@@ -8,20 +8,36 @@ import java.io.IOException;
 
 public class _04_ApachePOIGetAllData {
     public static void main(String[] args) throws IOException {
-        String path="src/test/java/ApachePOI/resource/ApacheExcel2.xlsx";
-        FileInputStream inputStream=new FileInputStream(path);
-        Workbook workbook= WorkbookFactory.create(inputStream);
-        Sheet sheet=workbook.getSheet("Sheet1");
+        String path = "src/test/java/ApachePOI/resource/ApacheExcel2.xlsx";
+        FileInputStream inputStream = new FileInputStream(path);
+        Workbook workbook = WorkbookFactory.create(inputStream);
+        Sheet sheet = workbook.getSheet("Sheet1");
 
         //zoo.length : kaç satır olduğunu
-        int rowCount=sheet.getPhysicalNumberOfRows(); // Kaç tane satır var
+        int rowCount = sheet.getPhysicalNumberOfRows(); // Kaç tane satır var
         System.out.println("rowCount = " + rowCount);
 
         for (int i = 0; i < rowCount; i++) {
+            Row row = sheet.getRow(i);
+            int cellCount = row.getPhysicalNumberOfCells();
+            for (int j = 0; j < cellCount; j++) {
+                System.out.print(row.getCell(j) + "\t");
+            }
+            System.out.println();
 
+        }// 2.Yol
+        for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
+
+            for (int j = 0; j < sheet.getRow(i).getPhysicalNumberOfCells(); j++) {
+
+                System.out.print(sheet.getRow(i).getCell(j)+"\t");
+            }
+            System.out.println();
 
 
         }
 
+
     }
 }
+
