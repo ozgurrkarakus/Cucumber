ed822074cd39b1017b4ea8f3a1bf0553752d02b2
ozgurrkarakus <ozgurkarakus.fb@gmail.com>
2024-05-16 22:36:36 +0300

    _05_Soru (Relative)

--- src/test/java/_JDBC/_05_Soru.java
+++ src/test/java/_JDBC/_05_Soru.java
@@ -5,22 +5,38 @@ import org.testng.annotations.Test;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class _05_Soru extends JDBCParent{
-@Test
+public class _05_Soru extends JDBCParent {
+    @Test
     public void test1() throws SQLException {
-    DBConnectionOpen();
+        DBConnectionOpen();
 
-    ResultSet rs=sorguEkrani.executeQuery("select city from city");
-rs.last();
-int sonSatir= rs.getRow();
-    for (int i = 1; i <=sonSatir ; i++) {
-     rs.absolute(i);
-        System.out.println(i+". Satir: "+rs.getString(1));
+        ResultSet rs = sorguEkrani.executeQuery("select city from city");
+        rs.last();
+        int sonSatir = rs.getRow();
+        for (int i = 1; i <= sonSatir; i++) {
+            rs.absolute(i);
+            System.out.println(i + ". Satir: " + rs.getString(1));
 
+        }
+
+        DBConnectionClose();
     }
 
-    DBConnectionClose();
+    @Test
+    public void test2() throws SQLException {
+        DBConnectionOpen();
 
-}
+        ResultSet rs = sorguEkrani.executeQuery("select city from city");
+        rs.last();
+        int sonSatir = rs.getRow();
+        rs.first();
+        System.out.println("1. Satir: "+rs.getString(1));
 
+        for (int i = 1; i <= sonSatir; i++) {
+            rs.relative(1);
+            System.out.println(i + ". Satir: " + rs.getString(1));
+
+        }
+        DBConnectionClose();
+    }
 }
